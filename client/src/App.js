8c1164cd3f05f44c165626bb5dd3c4a27ee50663
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-10 10:46:42 -0500

    created simple landing page and exported to app js, removed test state management code from app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,9 @@
-import { useAppContext } from "./context/appContext"
+import { Landing } from "./pages"
 
 const App = () => {
-  
-  const {arrayChange, numberChange, clearNumber, number, array} = useAppContext()
-
-  const elements = array.map((e, i) => {
-    return <p key={i}>{e}</p>
-  })
-
   return (
     <>
-      {elements}
-      <button type="button" onClick={() => arrayChange(5)}>Add To Array</button>
-      <p></p>
-      {number}
-      <p></p>
-      <button type="button" onClick={() => numberChange(3)}>Add To Number</button>
-      <p></p>
-      <button type="button" onClick={clearNumber}>Return to Zero</button>
+      <Landing/>
     </>
   )
 }
