35f84d536bc1dbb0bdfa65f9864b4f926c55d45a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-10 11:49:03 -0500

    completed preliminary build out of landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,28 @@
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/favicon.ico';
+
 const Landing = () => {
   return (
-    <h3>Landing</h3>
+    <main>
+        <nav>
+            <img src={logo} alt='landing logo' className='logo'/>
+        </nav>
+        <div className='container'>
+            <div className='info'>
+                <h1>
+                    Welcome to the Writers' Forum.
+                </h1>
+                <p>
+                    Let's write amazing works together.
+                </p>
+                <button className='btn btn-hero'> Login/Register</button>
+                &nbsp;&nbsp;<span className='landing-span'>Or View All Of The </span>&nbsp;&nbsp;
+                <button className='btn btn-hero'> Completed Works </button>
+            </div>
+            <p></p>
+            <img src={main} alt='event scene' className='img'/>
+        </div>
+    </main>
   )
 }
 export default Landing
