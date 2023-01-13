54ad87e54a76cc7524c6c3faaaa5e2bdc0a59b73
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-13 10:07:52 -0500

    set up initial non-return register page functionality

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import { useAppContext } from "../context/appContext";
 import { Logo, FormRow, Alert } from "../components";
 import { useState } from "react";
+import Wrapper from '../assets/wrappers/RegisterPage';
 
 const localState = {
     name: '',
@@ -10,11 +11,37 @@ const localState = {
   }
 
 const Register = () => {
-
+    const {showAlert} = useAppContext();
     const [values, setValues] = useState(localState)
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        // const {name, email, password, isMember} = values;
+        // if(!email || !password || (!isMember && !name)){
+        //   displayAlert();
+        //   return
+        // }
+        // const currentUser = {name, email, password};
+        // if(isMember){
+        //   loginUser(currentUser)
+        // } else {
+        //  registerUser(currentUser);
+        // }
+      }
+      
+      const toggleMember = () => {
+        setValues({...values, isMember: !values.isMember})
+      }
+      
+      const handleChange = e =>{
+        setValues({...values, [e.target.name]: e.target.value})
+      }    
+
   return (
-    <div>Register</div>
+    <Wrapper className="full-page">
+
+    </Wrapper>
   )
 }
 export default Register
