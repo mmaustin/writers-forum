87846e7734fbd6ad1b614c98029a892715e6dbca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-13 09:30:54 -0500

    removed test state, reducer, and actions from app context

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,60 +1,27 @@
 import React, { useReducer, useContext } from 'react';
 
-const testState = {
-    number: 0,
-    array: [],
+const initialState = {
+    isLoading: false,
+    showAlert: false,
+    alertText: '',
+    alertType: '',
 }
 
 const AppContext = React.createContext();
 
-const ADD_TO_ARRAY ='ADD_TO_ARRAY';
-const ADD_TO_NUMBER ='ADD_TO_NUMBER';
-const CLEAR_NUMBER ='CLEAR_NUMBER';
-
 
 const reducer = (state,action)=>{
-    if(action.type === ADD_TO_ARRAY){
-        return{
-            ...state,
-            array: [...state.array, action.payload],
-        }
-    }
-    if(action.type === ADD_TO_NUMBER){
-        return{
-            ...state,
-            number: state.number === 0 ? state.number + action.payload : state.number * action.payload,
-        }
-    }
-    if(action.type === CLEAR_NUMBER){
-        return{
-            ...state,
-            number: state.number = 0,
-        }
-    }
+
 }
 
 const AppProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, testState)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
-    const arrayChange = addition => {
-        dispatch({type: ADD_TO_ARRAY, payload: addition})
-    }
-
-    const numberChange = num => {
-        dispatch({type: ADD_TO_NUMBER, payload: num})
-    }
-
-    const clearNumber = () => {
-        dispatch({type: CLEAR_NUMBER})
-    }
 
     return(
         <AppContext.Provider
         value={{
           ...state,
-          arrayChange,
-          numberChange,
-          clearNumber
         }}
       >
         {children}
@@ -67,4 +34,4 @@ const AppProvider = ({children}) => {
     return useContext(AppContext)
   }
   
-  export { AppProvider, useAppContext }        
+  export { AppProvider, useAppContext, initialState }        
