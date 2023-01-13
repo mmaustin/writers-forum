b45854bd22b9f8673a9f9fe3d77ff49ae8f97e25
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-13 09:45:49 -0500

    set up local state in register component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,18 @@
+import { useAppContext } from "../context/appContext";
+import { Logo, FormRow, Alert } from "../components";
+import { useState } from "react";
 
+const localState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+  }
 
 const Register = () => {
+
+    const [values, setValues] = useState(localState)
+
   return (
     <div>Register</div>
   )
