6d4fc3d228cd9bdb6d8f0a8f4d031bf0d47cef23
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-16 09:58:56 -0500

    created error handleer middleware and imported into server js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const app = express();
 
-import notFoundMiddleware from './middleware/not-found';
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.get('/', (req, res)=>{
     res.send('Welcome')
 })
 
-app.use(notFoundMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 
