df983ca2d99b893818a52f4dabbcb6bce80ab14e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-16 09:44:18 -0500

    created not found.js middleware and imported into server file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const app = express();
 
+import notFoundMiddleware from './middleware/not-found';
+
 app.get('/', (req, res)=>{
     res.send('Welcome')
 })
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5001;
 
