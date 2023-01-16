13ae3e47039219a7cfb33dc172804bb0aed5745a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-16 12:47:06 -0500

    set up temporary auth and works controllers

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+
+
+const register = async (req, res) => {
+    res.send(201).json({msg: 'user created'})
+}
+const login = async (req, res) => {
+    res.send(20).json({msg: 'user logged in'})
+}
+const updateUser = async (req, res) => {
+    res.send(200).json({msg: 'user updated'})
+}
---
+++ controllers/works.js
@@ -0,0 +1,18 @@
+
+
+const createWork = async (reg,res) => {
+    res.send(201).json({msg: 'work created'});
+}
+
+const getWork = async (reg,res) => {
+    res.send(200).json({msg: 'work retrieved'});
+}
+const getAllWorks = async (reg,res) => {
+    res.send(201).json({msg: 'works retrieved'});
+}
+const updateWork = async (reg,res) => {
+    res.send(201).json({msg: 'work updated'});
+}
+const deleteWork = async (reg,res) => {
+    res.send(200).json({msg: 'work deleted'});
+}
