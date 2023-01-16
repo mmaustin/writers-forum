f7f53cf38907545a20910a15a86e287ba3c8de89
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-16 13:11:15 -0500

    imported and implemented auth and works routes in server file

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ dotenv.config();
 
 import connectDB from './db/connect.js';
 
+import authRouter from './routes/authRoutes.js'
+import worksRouter from './routes/worksRoutes.js'
+
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
-app.get('/', (req, res)=>{
-    res.send('Welcome')
-})
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/works', worksRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
