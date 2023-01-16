4ac9bc60eb7817b78bbd9e233cda42f2846d35d8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-16 10:25:57 -0500

    added mongoose connect file and imported to server js to connect server to mongo db

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+import connectDB from './db/connect.js';
+
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
@@ -15,6 +18,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}`)
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
