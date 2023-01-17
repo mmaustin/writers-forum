00fdb56e6a6be0d86c21349b6a19f129c8d144db
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-17 09:34:09 -0500

    created user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import validator from 'validator';
+// import bcrypt from 'bcryptjs';
+// import jwt from 'jsonwebtoken';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email',
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+    select: false,
+  },
+  favoriteGenre: {
+    type: String,
+    required: [true, 'Please state your favorite genre'],
+    trim: true,
+    maxlength: 25,
+  }
+})
+
+export default mongoose.model('User', UserSchema);
