c19a90f70b51022c73b1bceb314b78faa9a59de5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-18 09:26:33 -0500

    added create jwt instance method in user js

--- models/User.js
+++ models/User.js
@@ -38,6 +38,12 @@ UserSchema.pre('save', async function () {
     //if (!this.isModified('password')) return
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
   })
+}
 
 export default mongoose.model('User', UserSchema);
