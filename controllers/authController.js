30df3dd5ccb1829cce29de35e4cee0164873f7e9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-19 11:25:02 -0500

    set up login functionality in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,22 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send({msg: 'user logged in'})
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ email }).select('+password')
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token})
 }
 const updateUser = async (req, res) => {
     res.send({msg: 'user updated'})
