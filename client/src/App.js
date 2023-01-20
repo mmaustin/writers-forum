1260979fe5221f83c49e8e36e2174520be749085
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-20 13:10:05 -0500

    imported shared layout components to app js and implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Landing, Register, Error } from "./pages"
+import { AddWork, AllMyWorks, Profile, SharedLayout } from './pages/dashboard';
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing/>} />
+        <Route path='/'>
+          <Route path='my-works' element={<AllMyWorks/>} />
+          <Route path='add-work' element={<AddWork/>} />
+          <Route path='profile' element={<Profile/>} />
+        </Route>
+        <Route path='/landing' element={<Landing/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='*' element={<Error/>} />        
       </Routes>
