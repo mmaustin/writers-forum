8c8c0dd740c0c182eba12c0614a9a21e0eb9cc34
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-21 10:30:39 -0500

    built out the complete navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,38 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import {FaAlignLeft, FaUserCircle, FaCaretDown} from 'react-icons/fa'
+import { useAppContext } from '../context/appContext';
+import { useState } from 'react';
+import Logo from './Logo';
 
 const Navbar = () => {
+  const {logoutUser, toggleSidebar, user} = useAppContext();
+  const [showLogout, setShowLogout] = useState(false)  
   return (
     <Wrapper>
-        <h3>navbar</h3>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+            <button type='button' className='dropdown-btn' onClick={logoutUser}>
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
