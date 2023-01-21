e64344904cce74fa2ecaa681dbfa14a6f54ffb4e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-21 11:16:17 -0500

    built out small side bar component

--- client/src/components/SmallSideBar.js
+++ client/src/components/SmallSideBar.js
@@ -1,9 +1,28 @@
 import Wrapper from '../assets/wrappers/SmallSideBar';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
+import {useAppContext} from '../context/appContext';
 
 const SmallSideBar = () => {
+  const {showSidebar, toggleSidebar} = useAppContext();  
   return (
     <Wrapper>
-        <h3>Small Side Bar</h3>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   )
 }
