2725242b76b85997a3b675e822295ac37047beba
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-23 10:22:03 -0500

    built out the profile page component

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,6 +1,56 @@
+import { useState } from 'react';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import {useAppContext} from '../../context/appContext';
+import {FormRow, Alert} from '../../components';
+
+
 const Profile = () => {
+  const {user, showAlert, displayAlert, updateUser, isLoading} = useAppContext();
+
+  const [name, setName] = useState(user?.name);
+  const [email, setEmail] = useState(user?.email);
+  const [favoriteGenre, setfavoriteGenre] = useState(user?.favoriteGenre);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name || !email || !favoriteGenre){
+      displayAlert();
+      return
+    }
+    updateUser({name, email, favoriteGenre});
+  }
+
   return (
-    <h3>Profile</h3>
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        {showAlert && <Alert />}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='name'
+            value={name}
+            handleChange={(e) => setName(e.target.value)}
+          />
+          <FormRow
+            type='email'
+            name='email'
+            value={email}
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            type='text'
+            labelText='Favorite Genre'
+            name='favoriteGenre'
+            value={favoriteGenre}
+            handleChange={(e) => setfavoriteGenre(e.target.value)}
+          />
+          <button className='btn btn-block' type='submit' disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 export default Profile
