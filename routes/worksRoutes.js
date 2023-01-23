2489168b8fd2150b5e4e64c6df4414cc0877e98c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-23 09:00:01 -0500

    imported auth middleware to routes files and implemented where needed

--- routes/worksRoutes.js
+++ routes/worksRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
+import authenticateUser from '../middleware/auth.js';
 
 import {createWork, getWork, getAllWorks, updateWork, deleteWork} from '../controllers/worksController.js';
 
-router.route('/').get(getAllWorks).post(createWork);
-router.route('/:id').get(getWork).patch(updateWork).delete(deleteWork);
+router.route('/').get(getAllWorks).post(authenticateUser, createWork);
+router.route('/:id').get(authenticateUser, getWork).patch(authenticateUser, updateWork).delete(authenticateUser, deleteWork);
 
 export default router;
 
