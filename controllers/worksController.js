2e3fd68c938719ff140d113a83b1d110e3caba7a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-24 10:38:01 -0500

    created createWork function in works controller

--- controllers/worksController.js
+++ controllers/worksController.js
@@ -1,7 +1,16 @@
-
+import Work from "../models/Work.js";
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
 const createWork = async (reg,res) => {
-    res.send({msg: 'work created'});
+    const {title, genre, content, contributions} = req.body;
+
+    if(!title || !genre || !content || !contributions){
+        throw new BadRequestError('Please provide all values');
+    }
+    req.body.createdBy = req.user.userId;
+    const work = await Work.create(req.body);
+    res.status(StatusCodes.CREATED).json({work});
 }
 
 const getWork = async (reg,res) => {
