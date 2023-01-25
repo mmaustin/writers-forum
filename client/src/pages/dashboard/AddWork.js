9a4a95f8a841d2e658e984903f260b0510d42f3d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-25 09:40:02 -0500

    imported app context, form row, alert, and wrapper components to create work component

--- client/src/pages/dashboard/AddWork.js
+++ client/src/pages/dashboard/AddWork.js
@@ -1,4 +1,10 @@
+import { useAppContext } from "../../context/appContext";
+import {FormRow, Alert} from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const AddWork = () => {
+  
+
   return (
     <h3>Add Work</h3>
   )
