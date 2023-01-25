1d69648a62f1df4d6c692740e3c328be7a72cf7e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-25 09:49:12 -0500

    completed non-return portion of add work component, minus edit features

--- client/src/pages/dashboard/AddWork.js
+++ client/src/pages/dashboard/AddWork.js
@@ -3,7 +3,26 @@ import {FormRow, Alert} from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
 const AddWork = () => {
+  const {showAlert, displayAlert, isLoading, title, genre, content, contributions, handleChange, clearValues, createWork } = useAppContext();
   
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!title || !genre || !content || !contributions ){
+      displayAlert()
+      return
+    }
+    // if(isEditing){
+    //   editEvent();
+    //   return
+    // }
+    createWork();
+  }
+
+  const handleEventInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    handleChange({ name, value })
+  }  
 
   return (
     <h3>Add Work</h3>
