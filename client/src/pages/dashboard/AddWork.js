97eaebb7b43e7587f96376c4dd77abf22d47364b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-25 09:56:26 -0500

    completed return value for add work component, minus edit feature

--- client/src/pages/dashboard/AddWork.js
+++ client/src/pages/dashboard/AddWork.js
@@ -25,7 +25,58 @@ const AddWork = () => {
   }  
 
   return (
-    <h3>Add Work</h3>
+    <Wrapper>
+      <form className='form'>
+        {/* <h3>{isEditing ? 'Edit Event' : 'Add Event'}</h3> */}
+        <h3>Add Event</h3>
+        {showAlert && <Alert/>}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='title'
+            value={title}
+            handleChange={handleEventInput}
+          />
+          <FormRow
+            type='text'
+            name='genre'
+            value={genre}
+            handleChange={handleEventInput}
+          />
+            <FormRow
+              type='textarea'
+              name='content'
+              value={content}
+              handleChange={handleEventInput}
+            />
+          <FormRow
+            type='number'
+            name='contributions'
+            value={contributions}
+            handleChange={handleEventInput}
+          />
+          <div className='btn-container'>
+            <button
+              type='submit'
+              className='btn btn-block submit-btn'
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              submit
+            </button>
+            <button
+              className='btn btn-block clear-btn'            
+              onClick={(e) => {
+                e.preventDefault()
+                clearValues()
+              }}
+            >
+              clear
+            </button>
+          </div>          
+        </div>
+      </form>
+    </Wrapper>
   )
 }
 export default AddWork
