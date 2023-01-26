ff0a9feb0c7ce172ad75403b99db0d39e01cd3be
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-26 10:39:52 -0500

    built out initial work compoent return features

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,10 +1,36 @@
-import { useAppContext } from "../context/appContext";
+import { FaPlay, FaCalendarAlt } from 'react-icons/fa';
+import {IoMdPerson} from 'react-icons/io';
+import {FcClock} from 'react-icons/fc';
+//import { useAppContext } from "../context/appContext";
 import Wrapper from '../assets/wrappers/Work';
 import WorkInfo from "./WorkInfo";
 
-const Work = () => {
+const Work = ({_id, title, genre, content, contributions, createdBy, complete}) => {
+
   return (
-    <div>Work</div>
+    <Wrapper>
+      <div className='content'>
+        <div className='content-center'>
+          <WorkInfo icon={<IoMdPerson />} text={title} />
+          <WorkInfo icon={<FaPlay />} text={genre} />
+          <WorkInfo icon={<FaCalendarAlt />} text={contributions} />
+          <WorkInfo icon={<FcClock />} text={complete} />
+          <WorkInfo icon={<FcClock />} text={createdBy} />
+        </div>        
+        {/* <footer>
+          <div className='actions'>
+          <Link to='add-event' className='btn edit-btn' onClick={()=> setEditEvent(_id)}>Edit</Link>
+          <button className='btn delete-btn'type='button' onClick={()=> deleteEvent(_id)}>Delete</button>
+          </div>
+        </footer> */}
+        <main>
+          <div className='main-icon'>{content.charAt(0)}</div>
+          <div className='info'>
+            <h5>{content}</h5>
+          </div>
+        </main>
+      </div>
+    </Wrapper>
   )
 }
 export default Work
