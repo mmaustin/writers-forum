03086b34a5725f2e1cca3ab4e064f5e30c93d4ea
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-26 09:58:39 -0500

    imported necessary componets to works container, minus Loading compoennt

--- client/src/components/WorksContainer.js
+++ client/src/components/WorksContainer.js
@@ -1,4 +1,7 @@
-
+import { useEffect } from "react";
+import { useAppContext } from "../context/appContext";
+import Wrapper from '../assets/wrappers/WorksContainer';
+import Work from './Work';
 
 const WorksContainer = () => {
   return (
