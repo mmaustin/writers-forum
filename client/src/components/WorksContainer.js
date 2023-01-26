e6be2f586fdf257461dffa262962b403efec6ca2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-01-26 10:05:01 -0500

    completed build out of works container

--- client/src/components/WorksContainer.js
+++ client/src/components/WorksContainer.js
@@ -2,10 +2,39 @@ import { useEffect } from "react";
 import { useAppContext } from "../context/appContext";
 import Wrapper from '../assets/wrappers/WorksContainer';
 import Work from './Work';
+import Loading from "./Loading";
 
 const WorksContainer = () => {
+  const {works, getWorks, isLoading, totalWorks} = useAppContext();
+  
+  useEffect(() => {
+    getWorks()
+    // eslint-disable-next-line
+  }, [])
+
+  if(isLoading){
+    return <Loading center />
+  }
+
+  if(works.length === 0){
+    return(
+      <Wrapper>
+        <h3>No Works To Display . . . </h3>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>WorksContainer</div>
+    <Wrapper>
+      <h5>
+        {totalWorks} event{works.length > 1 && 's'} found
+      </h5>
+      <div className="events">
+        {works.map(work => {
+          return <Work key={work._id} {...work}/>
+        })}
+      </div>
+    </Wrapper>
   )
 }
 export default WorksContainer
