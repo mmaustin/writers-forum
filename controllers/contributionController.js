568f18b3129e9915371d863972ce9dbb069868c7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-01 10:11:29 -0500

    created shell contribution controllers

--- controllers/contributionController.js
+++ controllers/contributionController.js
@@ -1,4 +1,18 @@
-
+import Contribution from '../models/Contribution.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, NotFoundError } from '../error/index.js';
 import checkPermissions from "../utils/checkPermissions.js";
+
+const createContribution = async(req, res) => {
+    console.log('contribution created');
+}
+
+const getContributions = async (req, res) => {
+    console.log('contributions received');
+}
+
+const deleteContribution = async (req, res) => {
+    console.log('contribution deleted');
+}
+
+export {createContribution, getContributions, deleteContribution};
