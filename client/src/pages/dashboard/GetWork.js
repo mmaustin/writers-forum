4d1b41ad1dc1cedb6153615c239f7682eedc293f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-03 09:00:05 -0500

    added the two new contributions components to get work and removed fetching of contributions from that component

--- client/src/pages/dashboard/GetWork.js
+++ client/src/pages/dashboard/GetWork.js
@@ -1,28 +1,29 @@
 import { useEffect } from "react";
 import { Navigate} from "react-router-dom";
 import { useAppContext } from "../../context/appContext";
+import ContributionAdd from "../../components/ContributionAdd";
+import ContributionsContainer from "../../components/ContributionsContainer";
 
 const GetWork = () => {
   const {work, getWorkContributions, workContributions, totalWorkContributions} = useAppContext();
 
-  useEffect(() => {
-    getWorkContributions();
-  },[])
+  // useEffect(() => {
+  //   getWorkContributions();
+  // },[])
 
-  const contributions = workContributions.filter(contrib => contrib.createdBy === work._id)
-  const c = contributions.map((con,i)=>{
-    return <p key={i}>{con.content}</p>
-  })
+  // const contributions = workContributions.filter(contrib => contrib.createdBy === work._id)
+  // const c = contributions.map((con,i)=>{
+  //   return <p key={i}>{con.content}</p>
+  // })
 
   return (
     <>
-        {/* <h2>Do Not Refresh!!!</h2> */}
         {work.length === 0 ? (<Navigate to='/'/>) : <h3>{work.title}</h3> }
         {work.genre}
         {work.content}
-        {work.contributions}
         <p></p>
-        {c}
+        <ContributionsContainer workId={work._id}/>
+        <ContributionAdd/>
     </>
   )
 }
