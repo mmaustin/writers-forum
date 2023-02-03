d4a823c1f2d8a45c5d1530757f9e640271641d7a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-03 11:28:55 -0500

    built out the initial non-return portion of contribution add component

--- client/src/components/ContributionAdd.js
+++ client/src/components/ContributionAdd.js
@@ -1,4 +1,25 @@
-const ContributionAdd = () => {
+import { useAppContext } from "../context/appContext";
+import {FormRow, Alert} from '../components';
+import { useState } from "react";
+
+const ContributionAdd = ({originalAuthorId, createdBy}) => {
+    const {user, isLoading, showAlert, displayAlert, clearValues} = useAppContext();
+    const contributor = user.name;
+    const contributorId = user._id;
+    const [content, setContent] = useState('');
+
+    const onSetContent = e => setContent(e.target.value);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(!contributor || !contributorId || !content || !createdBy || !originalAuthorId){
+            displayAlert();
+            return
+        }
+        const newContribution = {contributor, contributorId, content, createdBy, originalAuthorId}
+        console.log(newContribution);
+    }
+
   return (
     <div>Add A Contribution Here</div>
   )
