16e7642aa1e27cfe5b60232425e4516294804679
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-06 09:33:55 -0500

    troubleshooting issue around using .filter to find the correct work instead of .find

--- client/src/pages/dashboard/GetWork.js
+++ client/src/pages/dashboard/GetWork.js
@@ -9,21 +9,25 @@ const GetWork = () => {
   const {works} = useAppContext();
   const params = useParams();
   const workId = params.id
+  const workArray = [];
   const w = works.filter(w => w._id === workId)
+  // workArray.push(work)
+  // console.log(workArray);
   const work = w.map((wo,i)=>{
     return <div>
       <p key={i+1}>{wo.title} **** {wo.genre} **** {wo.content}</p>
     </div>
   })
-  console.log(work);
   
 
   return (
-    <>  {work.length !== 0 ? <>
+     <> 
+        {work.length !== 0 ? <>
         {work}
         <p></p>
-        <ContributionsContainer {...work}/>
-        <ContributionAdd createdBy={work._id} originalAuthorId={work.createdBy}/> </>
+        <ContributionsContainer {...w[0]}/>
+        <ContributionAdd createdBy={w[0]._id} originalAuthorId={w[0].createdBy}/>
+         </>
         : <Navigate to='/'/>}
     </>
   )
