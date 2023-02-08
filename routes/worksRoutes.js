a494c1c5aa84a04e20071a1109ed51ad4157270a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-08 09:53:50 -0500

    added a get user works action creater and user works route

--- routes/worksRoutes.js
+++ routes/worksRoutes.js
@@ -2,9 +2,9 @@ import express from 'express';
 const router = express.Router();
 import authenticateUser from '../middleware/auth.js';
 
-import {createWork, getWork, getWorks, updateWork, deleteWork} from '../controllers/worksController.js';
+import {createWork, getWork, getWorks, userWorks, updateWork, deleteWork} from '../controllers/worksController.js';
 
-router.route('/').get(authenticateUser, getWorks).post(authenticateUser, createWork);
+router.route('/').get(authenticateUser, getWorks).get(authenticateUser, userWorks).post(authenticateUser, createWork);
 router.route('/:id').get(getWork).patch(authenticateUser, updateWork).delete(authenticateUser, deleteWork);
 
 export default router;
