4c4e81f908639088b4970e667e748e2bee8b451f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-02-18 09:19:53 -0500

    installed and applied four security packages on the backend

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import path from 'path';
 
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 import connectDB from './db/connect.js';
 
 import authRouter from './routes/authRoutes.js'
@@ -27,6 +31,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/works', worksRouter);
